886c3622d32b1e0bd301c6c4d242814b5a2cb581
YuminKim <dbals0@naver.com>
2025-05-15 14:38:35 +0900

    Update text in DashboardHeader on SchedulePage to emphasize site settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -4,7 +4,7 @@ import { DashboardHeader } from "@/components/dashboard-header"
 export default function SchedulePage() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="설정 관리" text="사이트의 설정을 관리하세요." />
+      <DashboardHeader heading="설정 관리" text="사이트의 설정을 관리하세요.!!!" />
     </DashboardShell>
   )
 }
